5d1811fd8e86fcb3cf5d7442732fbc51847b94e2
namletneg <nameltneg@163.com>
2014-12-09 16:43:57 +0800

    绑定验证方法
    绑定好了验证方法，只需给要认证的添加class就OK

--- static/js/forms.js
+++ static/js/forms.js
@@ -5,7 +5,7 @@ var Forms = {
     blur: function(event,regString,spanText,min,max){
         event.bind('blur',function(){
             var value = $.trim(event.val());
-            var span = event.next();
+            var span = event.siblings('span');
             var count = Forms.realLength(value);
             var regular = new RegExp(regString);
             if(count == 0){
@@ -64,10 +64,6 @@ var Forms = {
         return reCh.test(str);
     }
 };
-Forms.blur($('#name'),'^[A-Za-z0-9\u4e00-\u9fa5]+$' ,'请输入字母或数字或中文',5,25);
-Forms.blur($('#password'),'^\\w+$' ,'6-16个字符的字母加数字或下划线组合',6,16);
-Forms.blur($('#e-mail'),'^(\\w)+(\\.\\w+)*@(\\w)+((\\.\\w+)+)$','请输入正确格式的E-Mail');
-
 
 $('.mustFill:not([type=checkbox])').on('focus',function(){
     $(this).next().show();
@@ -86,22 +82,61 @@ $('.fill[type=checkbox]').on('click',function(){
 //Fill in the information detection
 $('form').bind('submit',function(){
     var $mustFill = $('form').find('.mustFill'),
-        $span,
-        id,top,type;
+        $span,$active,
+        top,type;
 
     if($mustFill[0]){
-        id = '#' + $mustFill[0].id;
-        top = $(id).offset().top - 200;
+        $active = $($mustFill[0]);
+        top = $active.offset().top - 200;
         type = $mustFill[0].getAttribute('type');
-        $span = $($mustFill[0]).siblings('span');
+        $span = $active.siblings('span');
 
         $span.addClass('red-font').html("<i class='icon-error'></i>此项为必填项</span>");
         $(window).scrollTop(top);
         if(type !== 'checkbox'){
             $mustFill[0].select();
+            $active.css({'border-color':'#f00','background-color':'#fff8ee'});
         } else{
             $span.show();
         }
         return false;               //Stop the form submit
     }
 });
+
+//绑定验证方法
+var beginFill = function(){
+    var $mustFill = $('.mustFill'),
+        len = $mustFill.length,
+        $active,$span,
+        type,i;
+    for(i = 0; i < len; i++){
+        $active = $($mustFill[i]);
+        type = $active.attr('type');
+        $span = $active.siblings('span');
+        if(type === 'text' || type === 'password'){
+            if ($active.val() !== '') {
+                $active.removeClass('mustFill');
+                $span.html("<i class='icon-ok'></i></span>").show();
+            }
+        }
+        if($active.attr('type') === 'checkbox'){
+            if($active[0].checked){
+                $active.removeClass('mustFill');
+                $span.html("<i class='icon-ok'></i></span>").show();
+            }
+        }
+        if($active.hasClass('number')){
+            Forms.blur($active,'^[0-9]*$','只能输入数字',1);
+        } else if($active.hasClass('idCard')){
+            Forms.blur($active,/(^\d{15}$)|(^\d{17}([0-9]|X)$)/,'请输入正确的身份证',1);
+        } else if($active.hasClass('email')){
+            Forms.blur($active,'^(\\w)+(\\.\\w+)*@(\\w)+((\\.\\w+)+)$','请输入正确格式的E-Mail',1);
+        } else if($active.hasClass('userName')){
+            Forms.blur($active,'^[A-Za-z0-9\u4e00-\u9fa5]+$' ,'请输入字母或数字或中文',5,25)
+        } else if($active.hasClass('userPassword')){
+            Forms.blur($active,'^\\w+$' ,'6-16个字符的字母加数字或下划线组合',6,16);
+        } else{
+            Forms.blur($active,'','此项为必填项',1);
+        }
+    }
+}();
